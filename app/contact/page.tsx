3fb60b649cc536e47a9b62a3ee891fc58c025d87
gdhillon2 <gman.dhillon@gmail.com>
2025-06-13 11:03:08 -0700

    fixed github link

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -4,7 +4,7 @@ export default function ContactPage() {
     return (
         <div className="flex flex-col items-center">
             <div className="flex max-w-[600px] gap-5">
-                <ContactCard tooltip="Go to GitHub" icon="github" link="https://github.com/gdhillon2"/>
+                <ContactCard tooltip="Go to GitHub" icon="github" link="https://github.com/gurvir-dhillon1"/>
                 <ContactCard tooltip="Go to LinkedIn" icon="linkedin" link="https://www.linkedin.com/in/gurvir-dhillon-802504295/"/>
                 <ContactCard tooltip="Copy email" icon="email" copy_string="gurvirdhillonswe@gmail.com" />
             </div>
