c8526477a74b87d2c0de07c43ec3b17ee164dbc0
gdhillon2 <gman.dhillon@gmail.com>
2025-11-07 14:39:40 -0500

    moved wasm draw to the top

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,12 @@ export default function Projects() {
         <div className="flex flex-col items-center w-full h-full bg-black base-text gap-5 p-5">
             <div className="animate-slide-in-up w-[100%] lg:w-[60%] h-fit grid grid-cols-1 gap-9 p-5">
             <ProjectCard 
+                projectTitle="WASM Draw" 
+                textContent="Drawing web app that uses C++ and WebAssembly to render the canvas, with WebSocket support for real-time collaborative drawing between clients." 
+                imgSrc="wasmdraw.svg"
+                href="https://wasm-draw.art"
+            />
+            <ProjectCard 
                 projectTitle="ResumeTailor" 
                 textContent="Resume builder that leverages AI to help you create a resume that gets more responses."
                 imgSrc="resumetailor.svg"
@@ -13,12 +19,6 @@ export default function Projects() {
                 outerBorderColor="border-neutral-900"
                 innerBorderColor="border-neutral-700"
                 href="https://resume-tailor-eta.vercel.app/"
-            />
-            <ProjectCard 
-                projectTitle="WASM Draw" 
-                textContent="Drawing web app that uses C++ and WebAssembly to render the canvas, with WebSocket support for real-time collaborative drawing between clients." 
-                imgSrc="wasmdraw.svg"
-                href="https://wasm-draw.art"
             />
             <ProjectCard 
                 projectTitle="Self Driving Car" 
