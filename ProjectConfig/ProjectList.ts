d2ff67a5b7c90e7fc6675dc7b344b6c635f3f82c
gdhillon2 <gman.dhillon@gmail.com>
2025-11-09 22:26:18 -0500

    create a list of projects that we will use to dynamically generate cards for

---
+++ ProjectConfig/ProjectList.ts
@@ -0,0 +1,29 @@
+const returnProject = (name: string, description: string, imgPath: string, href: string = '/'): Record<string, string> => {
+  return {
+    name: name,
+    description: description,
+    imgPath: imgPath,
+    href: href
+  }
+}
+
+export const ProjectList: Array<Record<string, any>> = [
+  returnProject(
+    'WASM Draw',
+    'Drawing web app that uses C++ and WebAssembly to render the canvas, with WebSocket support for real-time collaborative drawing between clients.',
+    'wasmdraw.svg',
+    'https://wasm-draw.art'
+  ),
+  returnProject(
+    'ResumeTailor',
+    'Resume builder that leverages AI to help you create a resume that gets more responses.',
+    'resumetailor.svg',
+    'https://resume-tailor-eta.vercel.app/'
+  ),
+  returnProject(
+    'Self Driving Car',
+    'Self driving car that uses IR and ultrasonic sensors to navigate a road with obstacles.',
+    'car.svg',
+    'https://github.com/gurvir-dhillon1/self-driving-car'
+  ),
+];
