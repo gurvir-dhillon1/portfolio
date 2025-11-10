328674027d59d418d78e947f354564e39df6f93b
gdhillon2 <gman.dhillon@gmail.com>
2025-11-09 22:26:53 -0500

    dynamically generate cards with alternating colors for each project in the project list

--- app/page.tsx
+++ app/page.tsx
@@ -1,35 +1,33 @@
 import ProjectCard from "@/components/ProjectCard";
-
+import { ProjectList } from "@/ProjectConfig/ProjectList";
 export default function Projects() {
+    const oddStyling: Record<string, string> = {
+        bgColor: 'dark:bg-neutral-900 bg-neutral-100',
+        outerBgColor: 'dark:bg-neutral-950',
+        outerBorderColor: 'dark:border-neutral-900',
+        innerBorderColor: 'dark:border-neutral-700'
+    };
+
+    const evenStyling: Record<string, string> = {
+        bgColor: 'dark:bg-slate-900 bg-slate-100',
+        outerBgColor:'dark:bg-slate-950',
+        outerBorderColor:'dark:border-slate-900',
+        innerBorderColor:'dark:border-slate-700',
+    }
     return (
         <div className="flex flex-col items-center w-full h-full bg-background base-text gap-5 p-5">
             <div className="animate-slide-in-up w-[100%] lg:w-[60%] h-fit grid grid-cols-1 gap-9 p-5">
-            <ProjectCard 
-                projectTitle="WASM Draw" 
-                textContent="Drawing web app that uses C++ and WebAssembly to render the canvas, with WebSocket support for real-time collaborative drawing between clients." 
-                imgSrc="wasmdraw.svg"
-                href="https://wasm-draw.art"
-            />
-            <ProjectCard 
-                projectTitle="ResumeTailor" 
-                textContent="Resume builder that leverages AI to help you create a resume that gets more responses."
-                imgSrc="resumetailor.svg"
-                bgColor="bg-neutral-900"
-                outerBgColor ="bg-neutral-950"
-                outerBorderColor="border-neutral-900"
-                innerBorderColor="border-neutral-700"
-                href="https://resume-tailor-eta.vercel.app/"
-            />
-            <ProjectCard 
-                projectTitle="Self Driving Car" 
-                textContent="Self driving car that uses IR and ultrasonic sensors to navigate a road with obstacles." 
-                imgSrc="car.svg"
-                bgColor="bg-slate-900"
-                outerBgColor="bg-slate-950"
-                outerBorderColor="border-slate-900"
-                innerBorderColor="border-slate-700"
-                href="https://github.com/gurvir-dhillon1/self-driving-car"
-            />
+                {ProjectList.map((info, index) => (
+                        <ProjectCard 
+                            projectTitle={info.name}
+                            textContent={info.description}
+                            outerBgColor={index & 1 ? oddStyling.outerBgColor: evenStyling.outerBgColor}
+                            outerBorderColor={index & 1 ? oddStyling.outerBorderColor: evenStyling.outerBorderColor}
+                            bgColor={index & 1 ? oddStyling.bgColor: evenStyling.bgColor}
+                            imgSrc={info.imgPath}
+                            href={info.href}
+                        />
+                    ))}
             </div>
         </div>
     )
